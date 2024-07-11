62a71a3b3582438ac519c3ed476dbe11b0e6030d
theodevoid <voidfnc9@gmail.com>
2024-07-11 17:46:46 +0700

    docs: add description for weather manager methods

--- NC3/Services/WeatherManager.swift
+++ NC3/Services/WeatherManager.swift
@@ -16,7 +16,11 @@ class WeatherManager {
     static let shared = WeatherManager()
     let service = WeatherService.shared
     
-    // Fetch hourly forecast
+    /// Returns the hourly forecast of a given location
+    ///
+    /// - Parameter location: The requested location
+    /// - Returns: The hourly forecast of a location
+    ///
     func hourlyForecast(for location: CLLocation) async -> Forecast<HourWeather>? {
         let hourlyForecast = await Task.detached(priority: .userInitiated) {
             let forecast = try? await self.service.weather(
@@ -28,7 +32,11 @@ class WeatherManager {
         return hourlyForecast
     }
     
-    // Function that filter hourly forecast into 6 - 8 am and 4 - 6 pm
+    /// Returns the hourly forecast of a given location between 6 - 8 AM and 4 - 6 PM
+    ///
+    /// - Parameter location: The requested location
+    /// - Returns: Hourly forecast of a location filtered only between 6 - 8 AM and 4 - 6 PM
+    ///
     func filteredHourlyForecast(for location: CLLocation) async -> [HourWeather]? {
         guard let hourlyForecast = await hourlyForecast(for: location) else {
             return nil
