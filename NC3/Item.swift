c5525dcfaa4830706bad73fdf478ec380c13d36f
theodevoid <voidfnc9@gmail.com>
2024-07-11 14:02:13 +0700

    Initial Commit

---
+++ NC3/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NC3
+//
+//  Created by Theodore Mangowal on 11/07/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NC3/NC3App.swift
@@ -0,0 +1,32 @@
+//
+//  NC3App.swift
+//  NC3
+//
+//  Created by Theodore Mangowal on 11/07/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NC3App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
