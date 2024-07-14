528e6e05ef79f99db45454c9db9de46eb3e585e9
Graceliora <noveliora.gracella@gmail.com>
2024-07-14 23:18:12 +0700

    docs: Add description to event manager

--- NC3/Services/EventManager.swift
+++ NC3/Services/EventManager.swift
@@ -11,12 +11,20 @@ import EventKit
 let eventStore: EKEventStore = EKEventStore()
 
 class EventManager {
+    
+    /// Request access to calendar
+    /// Save event to calendar
+    ///
+    /// - Parameter title: The location name
+    /// - Parameter date: The event date (still in String format)
+    ///
 
     func requestAccessAndSaveEvent(title: String, date: String) {
         eventStore.requestFullAccessToEvents { (granted, error) in
             if granted && error == nil {
                 print("Access granted: \(granted)")
                 
+                // Formatting String into date format
                 let formatter = DateFormatter()
                 formatter.dateFormat = "yyyy/MM/dd HH:mm"
                 let date = formatter.date(from: date)
