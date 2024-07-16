05ba8b24ee80e49340210f9be7428f12cf5ed3e0
theodevoid <voidfnc9@gmail.com>
2024-07-16 17:03:53 +0700

    feat: add description property

--- NC3/Models/Location.swift
+++ NC3/Models/Location.swift
@@ -12,12 +12,13 @@ class Location {
     var locationName: String
     var latitude: Double
     var longitude: Double
+    var description: String?
     
-    init(city: String, locationName: String, latitude: Double, longitude: Double) {
+    init(city: String, locationName: String, latitude: Double, longitude: Double, description: String? = nil) {
         self.city = city
         self.locationName = locationName
         self.latitude = latitude
         self.longitude = longitude
+        self.description = description
     }
 }
-
