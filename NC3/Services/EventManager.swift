806827b9cfd701a8b9ab7927da24866a2da9f228
Graceliora <noveliora.gracella@gmail.com>
2024-07-16 15:51:43 +0700

    func: Combined bookmark functionality & add to calendar

--- NC3/Services/EventManager.swift
+++ NC3/Services/EventManager.swift
@@ -18,27 +18,48 @@ class EventManager {
     /// - Parameter title: The location name
     /// - Parameter date: The event date (still in String format)
     ///
+    
+    var bool = false
 
-    func requestAccessAndSaveEvent(title: String, date: String) {
+    func requestAccessAndSaveEvent(title: String, date: Date, time: RecommendedLocationTime) {
+        
         eventStore.requestFullAccessToEvents { (granted, error) in
             if granted && error == nil {
                 print("Access granted: \(granted)")
-                
-                // Formatting String into date format
-                let formatter = DateFormatter()
-                formatter.dateFormat = "yyyy/MM/dd HH:mm"
-                let date = formatter.date(from: date)
 
                 let event = EKEvent(eventStore: eventStore)
                 event.title = title
-                event.startDate = date
-                event.endDate = date!.addingTimeInterval(60 * 60 * 2)
+                
+                let calendar = Calendar.current
+                
+                if (time == .morning) {
+                    var dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
+                    dateComponents.hour = 6
+                    dateComponents.minute = 0
+                    dateComponents.second = 0
+                    
+                    let morningDate = calendar.date(from: dateComponents)
+                    
+                    event.startDate = morningDate
+                    event.endDate = morningDate!.addingTimeInterval(60 * 60 * 2)
+                } else {
+                    var dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
+                    dateComponents.hour = 16
+                    dateComponents.minute = 0
+                    dateComponents.second = 0
+                    
+                    let afternoonDate = calendar.date(from: dateComponents)
+                    event.startDate = afternoonDate
+                    event.endDate = afternoonDate!.addingTimeInterval(60 * 60 * 2)
+                }
+            
                 event.notes = "This is a note"
                 event.calendar = eventStore.defaultCalendarForNewEvents
 
                 do {
                     try eventStore.save(event, span: .thisEvent)
                     print("Saved Event")
+                    self.bool = true
                 } catch let error as NSError {
                     print("Failed to save event with error: \(error)")
                 }
@@ -47,4 +68,5 @@ class EventManager {
             }
         }
     }
+    
 }
