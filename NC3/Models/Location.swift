82323185e3a9b10dd0279a3d069f06cb7ff45998
Graceliora <80310584+Graceliora@users.noreply.github.com>
2024-07-17 15:35:27 +0700

    chore: Add map links to locations list data (#14)

--- NC3/Models/Location.swift
+++ NC3/Models/Location.swift
@@ -13,12 +13,14 @@ class Location {
     var latitude: Double
     var longitude: Double
     var description: String?
+    var map: String
     
-    init(city: String, locationName: String, latitude: Double, longitude: Double, description: String? = nil) {
+    init(city: String, locationName: String, latitude: Double, longitude: Double, description: String? = nil, map: String) {
         self.city = city
         self.locationName = locationName
         self.latitude = latitude
         self.longitude = longitude
         self.description = description
+        self.map = map
     }
 }
